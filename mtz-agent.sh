eb99db2879c581fa970802b8daa625a7e3b60945
沉寂 <bianzhifu@gmail.com>
2023-03-10 23:53:42 +0800

    Update mtz-agent.sh

--- mtz-agent.sh
+++ mtz-agent.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-serveradd=$1
-serverp=$2
-bash <(curl -s https://raw.githubusercontent.com/bianzhifu/share/master/install.sh) mtz-agent install
-sed -i "s@ExecStart=/opt/mtz-agent/mtz-agent@ExecStart=/opt/mtz-agent/mtz-agent -s=\"${serveradd}\" -p=\"${serverp}\"@g" /etc/systemd/system/mtz-agent.service
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <serveradd> <serverp>"
+    exit 1
+fi
+
+SERVERADD="$1"
+SERVERP="$2"
+
+curl -s https://raw.githubusercontent.com/bianzhifu/share/master/install.sh | sudo bash -s mtz-agent install
+
+sed -i "s@ExecStart=/opt/mtz-agent/mtz-agent@ExecStart=/opt/mtz-agent/mtz-agent -s=\"$SERVERADD\" -p=\"$SERVERP\"@g" /etc/systemd/system/mtz-agent.service
+
 systemctl daemon-reload
 systemctl restart mtz-agent.service
